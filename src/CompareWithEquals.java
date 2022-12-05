4e6d469a17cf01189bbc49029b19a37852fe338d
Andrés Fernandez Pereira <animaccion96@gmail.com>
2022-12-05 18:21:04 +0100

    aordenando en paquetes

--- src/CompareWithEquals.java
+++ src/CompareWithEquals.java
@@ -1,16 +1,9 @@
 /**
  * @Author: Andrés Fernández Pereira
  */
-class Persona {
-    private final String nombre;
-    public Persona(String nombre) {
-        this.nombre = nombre;
-    }
 
-}
 
 public class CompareWithEquals {
-
     public static void main(String... argv) {
 
         Persona p = new Persona("Pepe");
@@ -20,12 +13,11 @@ public class CompareWithEquals {
         System.out.println(p.equals(p1)); // false
         System.out.println(p == p2); // true
         System.out.println(p.equals(p2)); // true
-
-
         String s1 = "hola";
         String s2 = s1;
         String s3 = "hola";
         String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+      .
 
         System.out.println(s1 == s2); // true
         System.out.println(s1 == s3); // true
@@ -33,7 +25,6 @@ public class CompareWithEquals {
         System.out.println(s1.equals(s2)); // true
         System.out.println(s1.equals(s3));  // true
         System.out.println(s1.equals(s4)); // true: referencias distintas a objetos iguales
-
     }
 }
 
